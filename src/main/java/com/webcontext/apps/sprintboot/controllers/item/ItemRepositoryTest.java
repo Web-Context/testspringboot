1303b3c0a32492b66355dc5f5a8a36d9ae6589e3
Frédéric Delorme <frederic.delorme@gmail.com>
2015-09-22 02:53:12 +0200

    Add an ItemRepository unit test.

---
+++ src/main/java/com/webcontext/apps/sprintboot/controllers/item/ItemRepositoryTest.java
@@ -0,0 +1,60 @@
+package com.webcontext.apps.sprintboot.controllers.item;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.webcontext.apps.sprintboot.Application;
+import com.webcontext.apps.sprintboot.entities.Item;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebIntegrationTest
+public class ItemRepositoryTest {
+
+	@Autowired
+	ItemRepository repo;
+
+	@Test
+	public void testFindAll() {
+		Item item1 = new Item(true, "Test 1");
+		Item item2 = new Item(true, "Test 2");
+		Item item3 = new Item(true, "Test 3");
+		repo.save(item1);
+		repo.save(item2);
+		repo.save(item3);
+		List<Item> items = repo.findAll();
+		assertEquals("Unable to retrieve items", 3, items.size());
+	}
+
+	@Test
+	public void testSaveS() {
+		Item item = new Item(true, "Test 1");
+		repo.save(item);
+		assertEquals("Unable to save item", true, item.getId() != null);
+	}
+
+	@Test
+	public void testDeleteT() {
+		Item item1 = new Item(true, "Test 1");
+		Item item2 = new Item(true, "Test 2");
+		repo.save(item1);
+		repo.save(item2);
+		int item2Id = item2.getId();
+
+		repo.delete(item2);
+
+		Item itemFind = repo.findOne(item2Id);
+
+		assertEquals("Unable to save item", null, itemFind);
+
+	}
+
+}
--- src/main/java/com/webcontext/apps/sprintboot/entities/Item.java
+++ src/main/java/com/webcontext/apps/sprintboot/entities/Item.java
@@ -5,24 +5,52 @@ import javax.persistence.Entity;
 
 @Entity
 public class Item extends SuperEntity {
-  @Column
-  private boolean checked;
-  @Column
-  private String description;
+	@Column
+	private boolean checked;
+	@Column
+	private String description;
 
-  public boolean isChecked() {
-    return checked;
-  }
+	public boolean isChecked() {
+		return checked;
+	}
 
-  public void setChecked(boolean checked) {
-    this.checked = checked;
-  }
+	public void setChecked(boolean checked) {
+		this.checked = checked;
+	}
 
-  public String getDescription() {
-    return description;
-  }
+	public String getDescription() {
+		return description;
+	}
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Item() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param checked
+	 * @param description
+	 */
+	public Item(boolean checked, String description) {
+		super();
+		this.checked = checked;
+		this.description = description;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Item [checked=").append(checked)
+				.append(", description=").append(description).append("]");
+		return builder.toString();
+	}
+
 }
